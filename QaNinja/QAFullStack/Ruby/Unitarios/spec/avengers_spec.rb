88d9a4b78d741eba0de1e7bc23596bcc78321682
trombeta <rodstrombeta@gmail.com>
2019-09-21 23:31:13 -0300

    TDD

---
+++ QaNinja/QAFullStack/Ruby/Unitarios/spec/avengers_spec.rb
@@ -0,0 +1,58 @@
+class AvengersHeadQuarter
+    attr_accessor :list
+
+    def initialize
+        self.list = []
+    end
+    
+    def put(avenger)
+        self.list.push(avenger)
+    end
+end
+
+# TDD (Desenvolviment guiado por testes)
+describe AvengersHeadQuarter do
+    it 'deve adicionar um vingador' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Spiderman')
+        expect(hq.list).to include 'Spiderman'
+    end
+
+    it 'deve adicionar uma lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        res = hq.list.size > 0
+
+        expect(hq.list.size).to be > 0
+        expect(res).to be true
+    end
+
+    it 'Thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'Ironman deve ser o ultimo da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+        expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/Trombeta/)
+    end
+end
