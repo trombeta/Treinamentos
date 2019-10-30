4902cb2351f50b21672db2d065d18b122c86b1f1
trombeta <rodstrombeta@gmail.com>
2019-10-30 12:55:37 -0300

    Cucumber para Sistemas Web - Aula 20 - Ferrari ou Fusca? BDD ou BTD?

--- QaNinja/QAFullStack/enjoeat/features/step_definitions/pedido_steps.rb
+++ QaNinja/QAFullStack/enjoeat/features/step_definitions/pedido_steps.rb
@@ -7,14 +7,14 @@ Dado("que eu feche o pedido com os itens:") do |table|
   end
   
   Dado("eu informo os dados de entrega:") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
+    user = table.rows_hash
+    @order_page.fill_user_data(user)
   end
   
-  Quando("eu finalizo o pedido com {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Quando("eu finalizo o pedido com {string}") do |payment|
+    @order_page.checkout(payment)
   end
   
-  Então("eu devo ver a seguinte mensagem:") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Então("eu devo ver a seguinte mensagem:") do |expect_message|
+    expect(@order_page.summary).to have_text expect_message
   end
