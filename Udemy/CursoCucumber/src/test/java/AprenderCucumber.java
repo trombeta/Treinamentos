58f0dd97b18afb19ac212c32815c39adc2a63f13
trombeta <rodstrombeta@gmail.com>
2019-11-04 21:11:51 -0300

    Curso Udemy BDD_Cucumber

---
+++ Udemy/CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,24 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dados;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class AprenderCucumber {
+	@Dados("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^executálo$")
+	public void executálo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Então("^a específicação deve fianalizar com sucesso$")
+	public void a_específicação_deve_fianalizar_com_sucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
---
+++ Udemy/CursoCucumber/src/test/java/aprender_cucumber.feature
@@ -0,0 +1,11 @@
+#language:pt
+
+Funcionalidade: Aprender Cucumber
+	Como um aluno
+	Eu quero aprender a utilizar Cucumber
+	Para que eu possa automatizar critérios de aceitação
+
+	Cenário: Deve executar específicação
+		Dado que criei o arquivo corretamente
+		Quando executálo
+		Então a específicação deve fianalizar com sucesso
