e6ad24081fb330b7e7e308058f0adc9ed340af45
Rodrigo Trombeta <rftrombeta@gmail.com>
2020-09-01 15:49:23 -0300

    Android - Part 1
    Curso de programação android - Alura

---
+++ Alura/Android/Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,30 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de Alunos");
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Rodrigo", "Fernando", "Trombeta"));
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+    }
+}
